b72db236a5d13a6c60f4b73149ccbdd9d9830e1f
Pasindu Dilshan <always.pasindu@gmail.com>
2025-03-20 21:38:02 +0530

    removed project configurations🤞

--- models/Apartment.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const ApartmentSchema = new mongoose.Schema({
-    apartmentName:{type:'String',required:true, unique:true},
-    createdAt:{type:Date, default:Date.now},
-});
-
-module.exports =  ApartmentSchema;
--- routes/apartmentsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const apartmentController = require('../controllers/apartmentController');
-
-// Fetch all apartment names
-router.get('/get-names', apartmentController.getAllApartments);
-
-module.exports = router;
